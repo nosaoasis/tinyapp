c67b5f5ee0740abf26cd35eaa0a63a635b4cf29f
Nosa Agho <nosaoasis@gmail.com>
2023-05-01 09:59:02 +0100

    Form Template and Route
    Implemented the form template to allow the creation of a new shortened url. Also added 'get' route to render the form template and a 'post' route to receive data from the form.

--- express_server.js
+++ express_server.js
@@ -3,10 +3,20 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
+};
+
+// random string function ......
+const generateRandomString = len => {
+  let generatedNumber = Math.random()
+    .toString(20)
+    .substr(2, `${len > 6 ? (len = 6) : (len = 6)}`);
+  return generatedNumber;
 };
 
 app.get("/", (req, res) => {
@@ -26,11 +36,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const { id } = req.params;
   const templateVars = { id, longURL: urlDatabase[id] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 
 app.get("*", (req, res) => {
   res
