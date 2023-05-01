9f1ddc1ccb9ea71fde69ac259366b3e7cb574521
Nosa Agho <nosaoasis@gmail.com>
2023-05-01 09:31:08 +0100

    Render Views
    Rendered a template and also passed values to the rendered template.

--- express_server.js
+++ express_server.js
@@ -21,6 +21,15 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("*", (req, res) => {
+  res.status(400).send("Sorry, page not found. <a href='/urls'>Back to home</a>")
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
