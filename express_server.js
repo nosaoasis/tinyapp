829b5fa6ccc186f2ae781b69c4e16cd09bdaae58
Nosa Agho <nosaoasis@gmail.com>
2023-05-01 15:29:14 +0100

    Basic Permission Features
    Implemented the basic permission for the login, register and the create new shortened url pages. A user cannot access those pages based on the status of their access.

--- express_server.js
+++ express_server.js
@@ -80,7 +80,7 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const userId = req.cookies["user_id"];
-  if (!userId) return res.redirect("/register");
+  if (!userId) return res.redirect("/login");
   const user = users[userId];
   const templateVars = {
     user,
@@ -92,6 +92,11 @@ app.get("/urls/:id", (req, res) => {
   const { id } = req.params;
   const userId = req.cookies["user_id"];
   if (!userId) return res.redirect("/register");
+  if(!urlDatabase[id]) return res
+  .status(400)
+  .send(
+    "<div><h2 style='text-align:center;'>404 Error</h2><p style='text-align:center;'>The page you're trying to access could not be found. Please check the url and try again</p><p style='text-align:center;'><a href='/urls'><button>Home</button></a></p>"
+  );
 
   const user = users[userId];
   const templateVars = {
@@ -124,7 +129,20 @@ app.get("/login", (req, res) => {
   res.status(200).render("login", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const {id} = req.params
+  let longURL = urlDatabase[id];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
+  const userId = req.cookies["user_id"];
+  if (!userId) return res
+      .status(400)
+      .send(
+        "<div><h2 style='text-align:center;'>Permission Denied</h2><p style='text-align:center;'>You must be logged in to shorten a url.</p><p style='text-align:center;'><a href='login'><button>Login</button></a></p>"
+      );
+
   const { longURL } = req.body;
   if (!longURL) {
     res.redirect("/urls/new");
